68a974731b7371d59897a2f300d6be71ba9bb815
Marshall210 <tymchenko99978@gmail.com>
2023-03-11 16:44:12 +0200

    adding audio on sphere

---
+++ Assets/Scripts/L51/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public string soundName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AudioManager.Instance.PlaySound(soundName, GetComponent<AudioSource>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
