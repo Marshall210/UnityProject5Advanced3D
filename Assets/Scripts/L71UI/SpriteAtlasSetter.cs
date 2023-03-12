54d6490b24dc671d8fc4c258a45f8ff37e1d6377
Marshall210 <tymchenko99978@gmail.com>
2023-03-12 19:45:02 +0200

    l7 adding UI, Vector Graphics, optimisation

---
+++ Assets/Scripts/L71UI/SpriteAtlasSetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SpriteAtlasSetter : MonoBehaviour
+{
+    public SpriteAtlas atlas; // Link to atlas
+    
+    private void Awake()
+    {
+        var image = GetComponent<Image>(); // Get image
+        image.sprite = atlas.GetSprite(image.sprite.name); // Change image to image from atlas
+    }
+}
---
+++ Assets/Scripts/L71UI/UpdateUi.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUi : MonoBehaviour
+{
+    public float speed; // Animation Speed 
+    public Graphic[] ui; // UI elements
+    private float[] uiAlpha; // Saved alpha channel of UI elements
+    private bool[] uiAlphaUpdate; // Saved process of elements
+
+    private void Awake()
+    {
+        uiAlpha = new float[ui.Length];
+        uiAlphaUpdate = new bool[ui.Length];
+        // Save alpha channel
+        for (var i = 0; i < ui.Length; i++)
+            uiAlpha[i] = ui[i].color.a;
+    }
+    
+    private void Update()
+    {
+        UpdateAlphaChannel();
+        //MoveRect();
+    }
+
+    // Move elements in random direction
+    private void MoveRect()
+    {
+        for (var i = 0; i < ui.Length; i++)
+        {
+            ui[i].rectTransform.anchoredPosition 
+                += new Vector2(Random.Range(-5f, 5f) * speed, Random.Range(-5f, 5f) * speed);
+        }
+    }
+
+    // Change alpha channel of UI elements
+    private void UpdateAlphaChannel()
+    {
+        for (var i = 0; i < ui.Length; i++)
+        {
+            if (!uiAlphaUpdate[i])
+            {
+                var color = ui[i].color;
+                color.a = Mathf.Lerp(color.a, 0, speed);
+                ui[i].color = color;
+
+                if (color.a < 0.001f)
+                    uiAlphaUpdate[i] = true;
+            }
+            else
+            {
+                var color = ui[i].color;
+                color.a = Mathf.Lerp(color.a, uiAlpha[i], speed);
+                ui[i].color = color;
+
+                if (color.a > uiAlpha[i] - 0.001f)
+                    uiAlphaUpdate[i] = false;
+            }
+        }
+    }
+}
