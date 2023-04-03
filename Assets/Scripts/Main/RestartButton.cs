c31a834d135993c156539a176f538fffc3e35a0f
Marshall210 <tymchenko99978@gmail.com>
2023-04-03 13:05:49 +0300

    menu and restart button

---
+++ Assets/Scripts/Main/RestartButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartButton : MonoBehaviour
+{
+    private Button button;
+
+    void Start()
+    {
+        // Получаем ссылку на компонент кнопки
+        button = GetComponent<Button>();
+
+        // Добавляем обработчик нажатия на кнопку
+        button.onClick.AddListener(RestartScene);
+    }
+
+    void RestartScene()
+    {
+        // Получаем номер текущей сцены и загружаем ее заново
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
